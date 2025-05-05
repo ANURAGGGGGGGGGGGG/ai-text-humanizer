4973e3d3252b05a244a6cf9379c976de2eeaf3f9
Anurag <144671360+ANURAGGGGGGGGGGGG@users.noreply.github.com>
2025-05-05 16:17:18 +0530

    Ai Humanizer

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import App from './App';
-import AppRoutes from "./routes/AppRoutes"
+import { HashRouter } from 'react-router-dom';
+import AppRoutes from './routes/AppRoutes';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <AppRoutes />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,29 +1,19 @@
-import {BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import TextHumanizer from '../components/TextHumanizer';
 import Contact from "../components/Contact";
-
 import About from '../components/About';
 import PrivacyPolicy from '../components/PrivacyPolicy';
 import TermsOfService from '../components/TermsOfService';
-// import Home from '../components/Home';
 
 const AppRoutes = () => {
   return (
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
-
     <Routes>
-
-      {/* <Route path="/" element={<App />} /> */}
-      {/* <Route path="/humanize" element={<TextHumanizer />} /> */}
       <Route index element={<TextHumanizer />} />
       <Route path="/contact" element={<Contact />} />
       <Route path="/about" element={<About />} />
       <Route path="/privacy" element={<PrivacyPolicy />} />
       <Route path="/terms" element={<TermsOfService />} />
-      {/*<Route path="*" element={<Navigate to="/" replace/>} /> */}
     </Routes>
-    </BrowserRouter>
-
   );
 };
 
